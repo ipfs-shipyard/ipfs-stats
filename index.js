f223836229203bbd126d9f40d19661512294f53c
Henrique Dias <hacdias@gmail.com>
2018-01-15 17:45:01 +0000

    fix: use poll instead of options

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ module.exports = class StatsPoller extends EventEmitter {
    * @return {Void}
    */
   _pollPeerStats () {
-    if (!this.options.peers) {
+    if (!this.poll.peers) {
       return
     }
 
